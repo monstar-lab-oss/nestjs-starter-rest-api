eab579ec96670518f298fb2c4fc3bac21a0d9f01
S M Asad Rahman <asad.rahman@monstar-lab.com.bd>
2020-07-23 12:40:46 +0600

    swagger doc setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /** Swagger configuration*/
+  const options = new DocumentBuilder()
+    .setTitle('Nestjs API starter')
+    .setDescription('Nestjs API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
