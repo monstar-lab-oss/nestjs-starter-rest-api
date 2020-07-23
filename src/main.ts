006b4dd36e806b2fc07337c3094b904f7c7b5c36
S M Asad Rahman <asad.rahman@monstar-lab.com.bd>
2020-07-23 13:06:16 +0600

    Auto generate swagger.json

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  /** Swagger configuration*/
+  /** Swagger configuration */
   const options = new DocumentBuilder()
     .setTitle('Nestjs API starter')
     .setDescription('Nestjs API description')
     .setVersion('1.0')
     .build();
 
+
   const document = SwaggerModule.createDocument(app, options);
+  fs.writeFileSync('./swagger.json', JSON.stringify(document, null, 2));
+
   SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
