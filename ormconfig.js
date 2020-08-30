95511141999f75d5e3a46e4472ddd362b40976e0
S M Asad Rahman <asad.rahman@monstar-lab.com.bd>
2020-08-31 02:46:22 +0600

    fix typeprm migration configuration

--- ormconfig.js
+++ ormconfig.js
@@ -5,8 +5,8 @@ module.exports = {
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  entities: [__dirname + '/src/**/*.entity{.ts,.js}'],
-  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+  entities: [__dirname + '/src/**/**/**.entity.ts'],
+  migrations: [__dirname + '/migrations/**'],
   migrationsRun: false,
   cli: {
     entitiesDir: 'src',
