416b586d55c536d162db596bc6672da17712cc86
sivanpayyadakath <sivan.payyadakath@monstar-lab.com>
2020-10-07 14:54:41 +0900

    feat: e2e tests for auth register

---
+++ test/auth/auth.e2e-spec.ts
@@ -0,0 +1,84 @@
+import * as request from 'supertest';
+import { Test } from '@nestjs/testing';
+import { AppModule } from './../../src/app.module';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
+import {
+  closeDBAfterTest,
+  createDBEntities,
+  resetDBBeforeTest,
+} from './../test-utils';
+import {
+  RegisterInput,
+  RegisterOutput,
+} from './../../src/auth/dto/register.dto';
+
+describe('AuthController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    await resetDBBeforeTest();
+    await createDBEntities();
+
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
+    await app.init();
+  });
+
+  it('/ (GET)', (done) => {
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Hello World!')
+      .end(() => done());
+  });
+
+  describe('register a new user', () => {
+    const registerInput: RegisterInput = {
+      name: 'e2etester',
+      email: 'e2etester@random.com',
+      password: '12345678',
+    };
+    const registerOutput: RegisterOutput = {
+      id: 1,
+      name: 'e2etester',
+      email: 'e2etester@random.com',
+    };
+
+    it('succesfully register a new user', () => {
+      return request(app.getHttpServer())
+        .post('/auth/register')
+        .send(registerInput)
+        .expect(HttpStatus.CREATED)
+        .expect(registerOutput);
+    });
+
+    it('register fails without Input DTO', () => {
+      return request(app.getHttpServer())
+        .post('/auth/register')
+        .expect(HttpStatus.BAD_REQUEST);
+    });
+
+    it('register fails when incorrect email format', () => {
+      registerInput.email = 'random';
+      return request(app.getHttpServer())
+        .post('/auth/register')
+        .expect(HttpStatus.BAD_REQUEST)
+        .send(registerInput)
+        .expect((res) => {
+          const resp = res.body;
+          expect(resp.error.message.message).toEqual([
+            'email must be an email',
+          ]);
+        });
+    });
+  });
+
+  afterAll(async () => {
+    await app.close();
+    await closeDBAfterTest();
+  });
+});
