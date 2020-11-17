a3fc1d15ff0e36f06c12ab3720b3fbc690b9219f
sivanpayyadakath <sivan.payyadakath@monstar-lab.com>
2020-11-17 12:35:36 +0900

    feat: swagger response auth endpoint

---
+++ src/shared/dtos/base-api-response.dto.ts
@@ -0,0 +1,54 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
+export class BaseApiResponse<T> {
+  public data: T; // Swagger Decorator is added in the extended class below, since that will override this one.
+
+  @ApiProperty({ type: Object })
+  public meta: any;
+}
+
+export function SwaggerBaseApiResponse<T>(type: T): typeof BaseApiResponse {
+  class ExtendedBaseApiResponse<T> extends BaseApiResponse<T> {
+    @ApiProperty({ type })
+    public data: T;
+  }
+  // NOTE : Overwrite the returned class name, otherwise whichever type calls this function in the last,
+  // will overwrite all previous definitions. i.e., Swagger will have all response types as the same one.
+  const isAnArray = Array.isArray(type) ? ' [ ] ' : '';
+  Object.defineProperty(ExtendedBaseApiResponse, 'name', {
+    value: `SwaggerBaseApiResponseFor ${type} ${isAnArray}`,
+  });
+
+  return ExtendedBaseApiResponse;
+}
+
+export class BaseApiErrorObject {
+  @ApiProperty({ type: Number })
+  public statusCode: number;
+
+  @ApiProperty({ type: String })
+  public message: string;
+
+  @ApiPropertyOptional({ type: String })
+  public localizedMessage: string;
+
+  @ApiProperty({ type: String })
+  public errorName: string;
+
+  @ApiProperty({ type: Object })
+  public details: unknown;
+
+  @ApiProperty({ type: String })
+  public path: string;
+
+  @ApiProperty({ type: String })
+  public requestId: string;
+
+  @ApiProperty({ type: String })
+  public timestamp: string;
+}
+
+export class BaseApiErrorResponse {
+  @ApiProperty({ type: BaseApiErrorObject })
+  public error: BaseApiErrorObject;
+}
--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -7,13 +7,17 @@ import {
   ClassSerializerInterceptor,
   HttpStatus,
 } from '@nestjs/common';
-
 import { Request } from 'express';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 import { UserService } from '../services/user.service';
 import { UserOutput } from '../dtos/user-output.dto';
 import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import {
+  BaseApiErrorResponse,
+  BaseApiResponse,
+  SwaggerBaseApiResponse,
+} from '../../shared/dtos/base-api-response.dto';
 
 @Controller('users')
 export class UserController {
@@ -22,9 +26,21 @@ export class UserController {
   @UseGuards(JwtAuthGuard)
   @UseInterceptors(ClassSerializerInterceptor)
   @Get('me')
-  @ApiOperation({ summary: 'Get user me API' })
-  @ApiResponse({ status: HttpStatus.OK, type: UserOutput })
-  getMyProfile(@Req() req: Request): Promise<UserOutput> {
-    return this.userService.findById(req.user['id']);
+  @ApiOperation({
+    summary: 'Get user me API',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: SwaggerBaseApiResponse(UserOutput),
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    type: BaseApiErrorResponse,
+  })
+  async getMyProfile(
+    @Req() req: Request,
+  ): Promise<BaseApiResponse<UserOutput>> {
+    const user = await this.userService.findById(req.user['id']);
+    return { data: user, meta: {} };
   }
 }
