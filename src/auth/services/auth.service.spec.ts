b9c7898a45957806e90c838ceac664de59de19bd
S M Asad Rahman <asad.rahman@monstar-lab.com>
2020-12-06 15:36:09 +0600

    test: auth service unit test for methods login, registration, validateUser

--- src/auth/services/auth.service.spec.ts
+++ src/auth/services/auth.service.spec.ts
@@ -4,14 +4,39 @@ import { JwtService } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { UserService } from '../../user/services/user.service';
 import { ConfigService } from '@nestjs/config';
+import { UnauthorizedException } from '@nestjs/common';
+import { User } from 'src/user/entities/user.entity';
+import { LoginOutput } from '../dtos/auth-login-output.dto';
+import { UserOutput } from 'src/user/dtos/user-output.dto';
+import { AuthToken } from '../dtos/token.dto';
 
 describe('AuthService', () => {
   let service: AuthService;
+
+  const mockedUserIdentity = { id: 6 };
+  const mockedUserInput = {
+    username: 'jhon',
+    name: 'Jhon doe',
+    password: 'any password',
+  };
+
+  const mockedUser = {
+    username: 'jhon',
+    name: 'Jhon doe',
+    ...mockedUserIdentity,
+  };
+
+  const mockedAuthToken = {
+    access_token: 'mock_access_token',
+    refresh_token: 'mock_access_token',
+  };
 
   const mockedUserService = {
     setContext: jest.fn(),
     log: jest.fn(),
     findById: jest.fn(),
+    createUser: jest.fn(),
+    validateUsernamePassword: jest.fn(),
   };
   const mockedJwtService = {
     setContext: jest.fn(),
@@ -37,27 +62,69 @@ describe('AuthService', () => {
     expect(service).toBeDefined();
   });
 
+  describe('validateUser', () => {
+    it('should success when username/ password valid', async () => {
+      jest
+        .spyOn(mockedUserService, 'validateUsernamePassword')
+        .mockImplementation(() => <UserOutput>mockedUser);
+
+      expect(await service.validateUser('jhon', 'somepass')).toEqual(
+        mockedUser,
+      );
+      expect(mockedUserService.validateUsernamePassword).toBeCalledWith(
+        'jhon',
+        'somepass',
+      );
+    });
+
+    it('should fail when username/ password invalid', async () => {
+      jest
+        .spyOn(mockedUserService, 'validateUsernamePassword')
+        .mockImplementation(() => {
+          throw new UnauthorizedException();
+        });
+
+      await expect(
+        service.validateUser('jhon', 'somepass'),
+      ).rejects.toThrowError();
+    });
+  });
+
+  describe('login', () => {
+    it('should return auth token for valid user', async () => {
+      jest
+        .spyOn(service, 'getAuthToken')
+        .mockImplementation(() => <LoginOutput>mockedAuthToken);
+
+      const authToken = await service.login(<User>mockedUser);
+
+      expect(service.getAuthToken).toBeCalledWith(mockedUser);
+      expect(authToken).toEqual(mockedAuthToken);
+    });
+  });
+
+  describe('register', () => {
+    it('should register new user', async () => {
+      jest
+        .spyOn(mockedUserService, 'createUser')
+        .mockImplementation(() => mockedUser);
+
+      const user = await service.register(mockedUserInput);
+
+      expect(mockedUserService.createUser).toBeCalledWith(mockedUserInput);
+      expect(user).toEqual(mockedUser);
+    });
+  });
+
   describe('refreshToken', () => {
-    const mockedUserIdentity = { id: 6 };
-    const mockedUser = {
-      username: 'jhon',
-      name: 'Jhon doe',
-      ...mockedUserIdentity,
-    };
-
-    const mockedAuthToken = {
-      access_token: 'mock_access_token',
-      refresh_token: 'mock_access_token',
-    };
-
     it('should generate auth token', async () => {
       jest
         .spyOn(mockedUserService, 'findById')
-        .mockImplementation(async () => mockedUser);
+        .mockImplementation(async () => <UserOutput>mockedUser);
 
       jest
         .spyOn(service, 'getAuthToken')
-        .mockImplementation(() => mockedAuthToken);
+        .mockImplementation(() => <AuthToken>mockedAuthToken);
 
       const authToken = await service.refreshToken(mockedUserIdentity);
 
