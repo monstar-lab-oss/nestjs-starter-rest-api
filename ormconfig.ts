32f30c991509db37435bcd6acbd1abd312a7e454
chavesfelipe <felipe.chavesdeoliveira@monstar-lab.com>
2022-08-08 22:56:33 +0900

    fix: ormconfig to datasource and fix path on package.json

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,10 +1,12 @@
-module.exports = {
+import { DataSource } from 'typeorm';
+
+const typeOrmConfig = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : null,
+  database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
   entities: [__dirname + '/src/**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
   migrationsRun: false,
@@ -17,4 +19,6 @@ module.exports = {
   timezone: 'Z',
   synchronize: false,
   debug: process.env.NODE_ENV === 'development' ? true : false,
-};
+});
+
+export default typeOrmConfig;
